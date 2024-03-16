690c76a65e9e24fa03e9433139a4f1d3a7a2a092
Gabriel Mendiola <gabriel.m.f.4@gmail.com>
2024-03-16 12:14:54 -0600

    Movimiento por medio de entrada de teclado

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Simulacion-Tutorial_Godot" 
---
+++ Sprite2D.gd
@@ -0,0 +1,20 @@
+extends Sprite2D
+
+var speed = 600
+var angular_speed = PI
+
+
+func _process(delta):
+	var direction = 0
+	if Input.is_action_pressed("ui_left"):
+		direction = -1
+	if Input.is_action_pressed("ui_right"):
+		direction = 1
+
+	rotation += angular_speed * direction * delta * 1.5
+
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("ui_up"):
+		velocity = Vector2.UP.rotated(rotation) * speed
+
+	position += velocity * delta
